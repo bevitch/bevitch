a7840e86a6893d29d6cfcecfb2594ef40faf1800
Szczepan Masny <szczepanmasny@gmail.com>
2020-05-12 00:32:48 +0200

    fix storybook

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,6 +1,6 @@
 import { addParameters } from '@storybook/vue';
 import { create } from '@storybook/theming/create';
-import logo from '../../src/assets/images/logo.png';
+import logo from '../../src/assets/images/logo.svg';
 import '../../src/styles/main.scss';
 
 addParameters({
