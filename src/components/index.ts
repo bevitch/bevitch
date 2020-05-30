cf3a438c7d5796809c3dd01067430ce0d2f8bcba
Agnieszka Ziomek <poziomkaa@gmail.com>
2020-05-30 18:58:56 +0200

    Add imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,15 @@
 import BvApp from './BvButton/BvButton.vue';
 import BvButton from './BvButton/BvButton.vue';
 import BvContentWrapper from './BvContentWrapper/BvContentWrapper.vue';
+import BvGrid from './BvGrid/BvGrid.vue';
+import BvGridItem from './BvGrid/BvGridItem/BvGridItem.vue';
 
 export {
   BvApp,
   BvButton,
-  BvContentWrapper
+  BvContentWrapper,
+  BvGrid,
+  BvGridItem
 };
+
+
