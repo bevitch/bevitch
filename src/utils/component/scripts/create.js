ee1212d84bad93af5d278622ed6e7d03e06bc9bb
Szczepan Masny <szczepanmasny@gmail.com>
2020-07-10 21:30:19 +0200

    fix

--- src/utils/component/scripts/create.js
+++ src/utils/component/scripts/create.js
@@ -56,7 +56,7 @@ const propmptForComponentName = () => {
         to: componentNamespace.length > 0 ? `${componentNamespace.join('/')}/${dirName}` : componentName
       });
       fs.appendFile(`${path}/index.ts`, `export * from './${dirName}';\n`, () => {});
-      fs.appendFile(`\n${path}/index.scss`, `@import './${dirName}';`, () => {});
+      fs.appendFile(`${path}/index.scss`, `\n@import './${dirName}';`, () => {});
       console.log(`${componentName} component has been created`);
     });
 };
